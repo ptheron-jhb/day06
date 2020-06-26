d38eb57e3d30a5e43b5f1963687ec5374af8cbe1
ptheron-jhb <67001715+ptheron-jhb@users.noreply.github.com>
2020-06-26 08:45:45 +0200

    Day06 activities
    Files not in respective directories

---
+++ ft_print_params.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptheron <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 11:48:50 by ptheron           #+#    #+#             */
+/*   Updated: 2020/06/22 11:50:14 by ptheron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int counter;
+	for(counter=0; counter<argc; counter++)
+		printf("argv[%2d]: %s\n",counter,argv[counter]);
+	return 0;
+}
